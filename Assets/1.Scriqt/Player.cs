6d869f0d47462ed613698141f9c998119714512b
chlwnsgh1883-droid <chlwnsgh1883@gmail.com>
2025-09-04 22:22:27 +0900

    002

--- Assets/1.Scriqt/Player.cs
+++ Assets/1.Scriqt/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 10;
     public Vector2 moveVecter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,8 +13,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        moveVecter = new Vector2(x, y);
+        float x = Input.GetAxisRaw ("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        moveVecter = new Vector2(x, y).normalized;
+
+        transform.position += (Vector3)moveVecter * speed * Time.deltaTime;
     }
 }
